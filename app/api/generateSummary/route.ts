07a08bddfe0773ca57d56d55c95d9a04579fc8cb
Vicky Prasad <prasadvicky231@gmail.com>
2023-12-26 03:20:35 +0530

    small fix

--- app/api/generateSummary/route.ts
+++ app/api/generateSummary/route.ts
@@ -23,7 +23,7 @@ export async function POST(request:Request){
         messages:[
             {
                 role: "system",
-                content:`when responding welcome the user always as Hello Raz and say welcome to the Workzone app!
+                content:`when responding welcome the user always as Hello and say welcome to the Workzone app!
                 Limit the response to 200 characters`
             },
             {
